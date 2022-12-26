4b45b70e84b8803fd1dda870deee6b0337c2c4cf
Bernarth <alberthbernarth75@gmail.com>
2022-12-26 18:37:18 -0400

    Add basics of classes

--- Typescript/class.js
+++ Typescript/class.js
@@ -1,25 +1,64 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Employee_id;
+// Creating Classes
 // Follow a Pascal convention
 // * If we don't wan to assign add ! (i.e. name!: string;)
+// * To define a private property I can use '#' or 'private'
+// * By default all memebers are public
+// * To define a protected memeber write protected
 class Employee {
+    // Static Members
+    static getEmployeeCount() {
+        return 50;
+    }
+    // We cannot have two constructors
     constructor(id, name, address) {
-        this.id = id;
+        // Class Properties and constructor
+        // Member Visibility
+        _Employee_id.set(this, void 0);
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
         this.name = name;
         this.address = address;
+    }
+    // Class Methods
+    getNameWithAddress() {
+        return `${this.name} stays at ${this.address}`;
+    }
+    // getters and setters
+    get empId() {
+        return __classPrivateFieldGet(this, _Employee_id, "f");
+    }
+    set empId(id) {
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
+    }
+}
+_Employee_id = new WeakMap();
+class Manager extends Employee {
+    constructor(id, name, address) {
+        super(id, name, address);
     }
     getNameWithAddress() {
         return `${this.name} stays at ${this.address}`;
     }
 }
-let john = new Employee(1, 'John', 'Highway 75');
-// john.id = 1;
-// john.name = 'John';
-// john.address = 'Highway 75';
-let information = john.getNameWithAddress();
-console.log(john, information);
-// Creating Classes
-// Class Properties and constructor
-// Class Methods
 // Creating Instance of Class
-// Member Visibility
-// Static Members
+let john = new Employee(1, 'John', 'Highway 75');
+let information = john.getNameWithAddress();
+let mike = new Manager(2, 'Mike', 'Cherise Drive');
+console.log(john, information);
+console.log(mike);
+// use getters and setters
+john.empId = 100;
+console.log(john.empId);
+// A class will be responsible to handle some bussiness logic.
--- Typescript/class.ts
+++ Typescript/class.ts
@@ -1,16 +1,46 @@
+// Creating Classes
 // Follow a Pascal convention
 // * If we don't wan to assign add ! (i.e. name!: string;)
 // * To define a private property I can use '#' or 'private'
+// * By default all memebers are public
+// * To define a protected memeber write protected
 class Employee {
-    id: number;
-    name: string;
+    // Class Properties and constructor
+    // Member Visibility
+    #id: number;
+    protected name: string;
     address: string;
+
+    // Static Members
+    static getEmployeeCount(): number{
+        return 50;
+    }
 
     // We cannot have two constructors
     constructor(id: number, name: string, address: string) {
-        this.id = id;
+        this.#id = id;
         this.name = name;
         this.address = address;
+    }
+
+    // Class Methods
+    getNameWithAddress(): string {
+        return `${this.name} stays at ${this.address}`;
+    }
+
+    // getters and setters
+    get empId(): number {
+        return this.#id;
+    }
+
+    set empId(id: number) {
+        this.#id = id;
+    }
+}
+
+class Manager extends Employee{
+    constructor(id: number, name: string, address: string) {
+        super(id, name, address);
     }
 
     getNameWithAddress(): string {
@@ -18,15 +48,17 @@ class Employee {
     }
 }
 
+// Creating Instance of Class
 let john = new Employee(1, 'John', 'Highway 75');
 let information = john.getNameWithAddress();
 
+let mike = new Manager(2, 'Mike', 'Cherise Drive');
+
 console.log(john, information);
+console.log(mike);
 
+// use getters and setters
+john.empId = 100;
+console.log(john.empId);
 
-// Creating Classes
-// Class Properties and constructor
-// Class Methods
-// Creating Instance of Class
-// Member Visibility
-// Static Members
+// A class will be responsible to handle some bussiness logic.

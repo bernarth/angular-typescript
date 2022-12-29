052ab79f58918abe533dee4e03e289482c67d41d
Bernarth <alberthbernarth75@gmail.com>
2022-12-29 00:15:47 -0400

    Built-in directives

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -8,8 +9,43 @@ import { Component } from '@angular/core';
 export class RoomsComponent {
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
-
   hideRooms = false;
+  
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://unsplash.com/es/fotos/9Eb97-TrMrA',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021')
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1000,
+      photos: 'https://unsplash.com/es/fotos/9Eb97-TrMrA',
+      checkinTime: new Date('12-Nov-2021'),
+      checkoutTime: new Date('13-Nov-2021')
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 1500,
+      photos: 'https://unsplash.com/es/fotos/9Eb97-TrMrA',
+      checkinTime: new Date('12-Nov-2021'),
+      checkoutTime: new Date('13-Nov-2021')
+    }
+  ];
 
   constructor() {}
 

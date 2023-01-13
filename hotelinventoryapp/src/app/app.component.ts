4a13261204dff938f2e921defa1b2f8e74147bb5
Bernarth <alberthbernarth75@gmail.com>
2023-01-12 22:23:12 -0400

    Create employee component and add content projection

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
-import { RoomsComponent } from './rooms/rooms.component';
+import { Component, ElementRef, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+// import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit{
   title = 'hotelinventoryapp';
   // role = 'Admin'; // Admin, User, None
 
-  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+  @ViewChild('name', {static: true}) name!: ElementRef;
 
-  ngAfterViewInit(): void {
-    // We loadded the component dynamically
-    const componentRef = this.vcr.createComponent(RoomsComponent);
-    componentRef.instance.numberOfRooms = 50;
+  ngOnInit(): void {
+    this.name.nativeElement.innerText = 'Hilton Hotel';
   }
+
+  // @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+
+  // ngAfterViewInit(): void {
+  //   // We loadded the component dynamically
+  //   const componentRef = this.vcr.createComponent(RoomsComponent);
+  //   componentRef.instance.numberOfRooms = 50;
+  // }
 }

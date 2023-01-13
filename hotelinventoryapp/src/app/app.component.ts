d9b6ca442fbff1718143b2fc3c81a15acb80b316
Bernarth <alberthbernarth75@gmail.com>
2023-01-12 21:39:15 -0400

    Load a component dynamically with View Child

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'hotelinventoryapp';
   // role = 'Admin'; // Admin, User, None
+
+  @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
+
+  ngAfterViewInit(): void {
+    // We loadded the component dynamically
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRooms = 50;
+  }
 }
